c4511e69c015ca7aef31d4b9be77767365cc56d1
Szabolcs Simonyi <szabolcs.simonyi@hotmail.com>
2016-09-02 22:22:30 +0200

    finalize migration

--- migrations/migration.php
+++ migrations/migration.php
@@ -1,13 +1,13 @@
 <?php
-
+header('charset=ISO-8859-2');
 class MigrationExecutor{
     private $statements=[];
+    private $resources=[];
     private $config;
     function __construct() {
         $this->config=require(__DIR__ . '/../config/main.php');
-        foreach(glob('./updates/*.sql') as $path){
-            $this->statements=array_merge($this->statements,  explode(";",file_get_contents($path)));
-        }
+        $this->loadStatements();
+        $this->loadResources();
     }
     
     private function connect(){
@@ -16,13 +16,57 @@ class MigrationExecutor{
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             $db->exec("DROP DATABASE IF EXISTS `{$this->config['db']}`");
-            $db->exec("CREATE DATABASE `{$this->config['db']}` /*!40100 DEFAULT CHARACTER SET utf8mb4 */");
+            $db->exec("CREATE DATABASE `{$this->config['db']}`  /*!40100 DEFAULT CHARACTER SET utf8  COLLATE utf8_hungarian_ci */");
             $db->exec("use `{$this->config['db']}`");
             return $db;
         } catch (PDOException $ex) {
             echo $ex->getMessage();
             return false;
-        }            
+        }
+    }
+    
+    private function loadStatements(){
+        foreach(glob('./updates/*.sql') as $path){
+            $this->statements=array_merge($this->statements,  explode(";",file_get_contents($path)));
+        }
+    }
+    
+    private function loadResources(){
+        foreach (glob('./updates/tables/*.csv') as $file){
+            list($path,$pos)=  explode('_', basename($file,".csv"));
+            $this->resources[$pos]=$file;
+        }
+        ksort($this->resources);
+    }            
+    
+    private function executeStatements($db){
+        foreach($this->statements as $statement){
+           if(strlen(trim($statement))===0){
+               continue;
+           }
+           echo PHP_EOL.$statement. PHP_EOL. PHP_EOL;
+           echo 'executed, rows affected: '.$db->exec($statement). PHP_EOL. PHP_EOL;
+        }
+    }
+    
+    private function executeResourses($db){
+        foreach($this->resources as $resource){
+            $content=file_get_contents($resource);
+            $table=explode('_',basename($resource,'.csv'))[0];
+            $rows=explode(PHP_EOL, $content);
+            $header=array_shift($rows);
+            $header=  '`'.implode("`,`", explode(";", $header)).'`';
+            echo "insert rows into {$table}".PHP_EOL;
+            foreach ($rows as $row){
+                $cells=  explode(';', $row);
+                if(count(explode(',', $header)) !== count($cells)){
+                    continue;
+                }
+                $str="INSERT INTO {$table}({$header}) VALUES('".implode("','", $cells)."')";
+                echo $str.PHP_EOL;
+                $db->exec($str);
+            }
+        }
     }
     
     public function execute(){
@@ -34,13 +78,8 @@ class MigrationExecutor{
             
             $db->beginTransaction();
 
-            foreach($this->statements as $statement){
-                if(strlen(trim($statement))===0){
-                    continue;
-                }
-                echo PHP_EOL.$statement. PHP_EOL. PHP_EOL;
-                echo 'executed, rows affected: '.$db->exec($statement). PHP_EOL. PHP_EOL;
-            }
+            $this->executeStatements($db);            
+            $this->executeResourses($db);
             
             $db->commit();
             
--- migrations/updates/init20160901.sql
+++ migrations/updates/init20160901.sql
@@ -1,24 +1,58 @@
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` enum('act','temp','del') NOT NULL,
   `email` varchar(45) DEFAULT NULL,
   `name` varchar(45) NOT NULL,
   `lang` varchar(2) DEFAULT 'EN',
   `created` datetime NOT NULL,
   `modified` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `i_users_states` (`status`),
+  KEY `i_users_emails` (`email`),
+  KEY `i_users_names` (`name`),
+  KEY `i_users_created` (`created`),
+  KEY `i_users_modified` (`modified`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `user_info` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `userinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `lang` varchar(2) DEFAULT 'EN',
   `aboutme` text,
   `birthdate` date DEFAULT NULL,
   `hometown` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `i_birthdate` (`birthdate`),
-  KEY `i_hometown` (`hometown`),
-  KEY `fk_user_idx` (`user_id`),
-  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+  KEY `i_userinfo_birthdate` (`birthdate`),
+  KEY `i_userinfo_hometown` (`hometown`),
+  KEY `fk_userinfo_user_idx` (`user_id`),
+  CONSTRAINT `fk_userinfo_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `forms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `desc` text,
+  `created` datetime NOT NULL,
+  `lang` varchar(2) DEFAULT 'EN',
+  `user_id` int(11) NOT NULL,
+  `downloads` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `i_forms_title` (`title`),
+  KEY `i_forms_created` (`created`),
+  KEY `i_forms_downloads` (`downloads`),
+  KEY `fk_forms_users_idx` (`user_id`),
+  CONSTRAINT `fk_forms_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `downloads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `form_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_downloads_forms_idx` (`form_id`),
+  KEY `i_downloads_created` (`created`),
+  KEY `fk_downloads_users_idx` (`user_id`),
+  CONSTRAINT `fk_downloads_forms` FOREIGN KEY (`form_id`) REFERENCES `forms` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_downloads_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
